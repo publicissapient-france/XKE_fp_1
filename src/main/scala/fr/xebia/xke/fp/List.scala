a3830a843dd7c083db584033283c2202a7212660
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2015-01-22 09:54:30 +0100

    Exo3 : drop

--- src/main/scala/fr/xebia/xke/fp/List.scala
+++ src/main/scala/fr/xebia/xke/fp/List.scala
@@ -2,11 +2,17 @@ package fr.xebia.xke.fp
 
 
 trait List[A]{
-	def tail:List[A]
+	def tail:List[A] = drop(1)
+
+	def drop(n: Int): List[A]
 }
 
-case class Cons[A](elt: A, tail:List[A]) extends List[A]
+case class Cons[A](a: A, as:List[A]) extends List[A]{
+	def drop(n:Int) = if (n < 1) {
+		this
+	} else as.drop(n-1)
+}
 
 case class Nil[A] extends List[A]{
-	def tail = this
+	def drop(n:Int) = this
 }
--- src/test/scala/fr/xebia/xke/fp/ListSpec.scala
+++ src/test/scala/fr/xebia/xke/fp/ListSpec.scala
@@ -24,12 +24,30 @@ class ListSpec extends FunSpec with Matchers {
 			new Nil[String].tail should be(new Nil[String])
 		}
 
+		it("returns itself when try to drop some element"){
+			new Nil[String].drop(2) should be(new Nil[String])	
+		}
+
 	 describe("A non-empty list"){
 	 	it("has a tail which is the list minus the first element"){
-	 		val list = new Cons(1,new Cons(2,new Nil[Int]))	
+	 		val list = new Cons(1,new Cons(2,new Nil[Int]))
 	 		val listTail = list.tail
 
 	 		listTail should be(new Cons(2,new Nil[Int]))
+	 	}
+
+	 	it("returns a sublist by dropping elements from the left"){
+	 		val list = new Cons(1,new Cons(2,new Cons(3,new Nil[Int])))
+	 		val sublist = list.drop(2)
+
+	 		sublist should be(new Cons(3,new Nil[Int]))
+	 	}
+
+	 	it("returns Nil when dropping more elements than its length"){
+	 		val list = new Cons(1,new Nil[Int])
+	 		val sublist = list.drop(3)
+
+	 		sublist should be(new Nil[Int])
 	 	}
 	 }
 	}
