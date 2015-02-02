d27e917cfcf69f427dde2fc86beb4e0c228f404d
Clément HELIOU <cheliou@xebia.fr>
2015-02-02 09:35:04 +0100

    chore: remove implementations & disable UT.

--- src/test/scala/fr/xebia/xke/fp/ListSpec.scala
+++ src/test/scala/fr/xebia/xke/fp/ListSpec.scala
@@ -1,111 +1,110 @@
 package fr.xebia.xke.fp
 
-import org.scalatest.FunSpec
-import org.scalatest.Matchers
+import org.scalatest.{FunSpec, Matchers}
 
 class ListSpec extends FunSpec with Matchers {
-
-	describe("A list"){
-		it("can be empty"){
-			Nil
-		}
-
-		it("can have one element"){
-			 Cons(1,Nil)
-		}
-
-		it("can have more than 1 element"){
-			 Cons(1, Cons(2,Nil))
-		}
-	}
-
-	describe("An empty list"){
-		it("has no tail"){
-			Nil.tail should be(Nil)
-		}
-
-		it("returns itself when try to drop some element"){
-			Nil.drop(2) should be(Nil)	
-		}
-		it("has no init"){
-			Nil.init should be(Nil)
-		}
-		it("as a sum of 0"){
-			List.sum(Nil) should be(0)
-		}
-		it("as a sum of 1"){
-			List.product(Nil) should be(1)
-		}
-		it("has a length of 0"){
-			Nil.length should be(0)
-		}
-	}
-
-	 describe("A non-empty list"){
-	 	it("has a tail which is the list minus the first element"){
-	 		val list = Cons(1, Cons(2,Nil))
-	 		val listTail = list.tail
-
-	 		listTail should be( Cons(2,Nil))
-	 	}
-
-	 	it("returns a sublist by dropping elements from the left"){
-	 		val list = Cons(1, Cons(2, Cons(3,Nil)))
-	 		val sublist = list.drop(2)
-
-	 		sublist should be( Cons(3,Nil))
-	 	}
-
-	 	it("returns Nil when dropping more elements than its length"){
-	 		val list = Cons(1,Nil)
-	 		val sublist = list.drop(3)
-
-	 		sublist should be(Nil)
-	 	}
-	 	it("init is the same list without the last one"){
-			val list = Cons(1, Cons(2, Cons(3,Nil)))
-			val init = list.init
-
-			init should be( Cons(1, Cons(2,Nil)))
-		}
-		it("can sum a list of int"){
-			List.sum(Cons(1, Cons(2, Cons(3, Nil)))) should be(6)
-		}
-		it("can product a list of int"){
-			List.product(Cons(2, Cons(3, Cons(4, Nil)))) should be(24)
-		}
-		it("has a length"){
-			Cons(2,Cons(3,Nil)).length should be(2)
-		}
-		it("add one to a list of ints"){
-			val result = List.addOne(Cons(2,Cons(3,Nil)))
-
-			result should be(Cons(3,Cons(4,Nil)))
-		}
-		it("can render elements as strings"){
-			val result = List.tostring(Cons(1,Cons(2,Nil)))
-
-			result should be(Cons("1",Cons("2",Nil)))
-		}
-		it("can be filtered"){
-			val evens = Cons(1,Cons(2,Cons(3,Nil))).filter(p => p % 2 == 0)
-
-			evens should be(Cons(2,Nil))
-		}	
-		it("can appended to another list"){
-			val list1 = Cons(1,Cons(2,Nil))
-			val list2 = Cons(3,Nil)
-
-			val result = List.append(list1,list2)
-
-			result should be(Cons(1,Cons(2,Cons(3,Nil))))
-		}
-		it("can be flatMapped"){
-			val list = Cons(1,Cons(2,Nil))
-
-			val result = list.flatMap(x => Cons(0,Cons(x,Nil)))
-
-			result should be(Cons(0,Cons(1,Cons(0,Cons(2,Nil)))))
-		}
-	 }
+  //
+  //	describe("A list"){
+  //		it("can be empty"){
+  //			Nil
+  //		}
+  //
+  //		it("can have one element"){
+  //			 Cons(1,Nil)
+  //		}
+  //
+  //		it("can have more than 1 element"){
+  //			 Cons(1, Cons(2,Nil))
+  //		}
+  //	}
+  //
+  //	describe("An empty list"){
+  //		it("has no tail"){
+  //			Nil.tail should be(Nil)
+  //		}
+  //
+  //		it("returns itself when try to drop some element"){
+  //			Nil.drop(2) should be(Nil)
+  //		}
+  //		it("has no init"){
+  //			Nil.init should be(Nil)
+  //		}
+  //		it("as a sum of 0"){
+  //			List.sum(Nil) should be(0)
+  //		}
+  //		it("as a sum of 1"){
+  //			List.product(Nil) should be(1)
+  //		}
+  //		it("has a length of 0"){
+  //			Nil.length should be(0)
+  //		}
+  //	}
+  //
+  //	 describe("A non-empty list"){
+  //	 	it("has a tail which is the list minus the first element"){
+  //	 		val list = Cons(1, Cons(2,Nil))
+  //	 		val listTail = list.tail
+  //
+  //	 		listTail should be( Cons(2,Nil))
+  //	 	}
+  //
+  //	 	it("returns a sublist by dropping elements from the left"){
+  //	 		val list = Cons(1, Cons(2, Cons(3,Nil)))
+  //	 		val sublist = list.drop(2)
+  //
+  //	 		sublist should be( Cons(3,Nil))
+  //	 	}
+  //
+  //	 	it("returns Nil when dropping more elements than its length"){
+  //	 		val list = Cons(1,Nil)
+  //	 		val sublist = list.drop(3)
+  //
+  //	 		sublist should be(Nil)
+  //	 	}
+  //	 	it("init is the same list without the last one"){
+  //			val list = Cons(1, Cons(2, Cons(3,Nil)))
+  //			val init = list.init
+  //
+  //			init should be( Cons(1, Cons(2,Nil)))
+  //		}
+  //		it("can sum a list of int"){
+  //			List.sum(Cons(1, Cons(2, Cons(3, Nil)))) should be(6)
+  //		}
+  //		it("can product a list of int"){
+  //			List.product(Cons(2, Cons(3, Cons(4, Nil)))) should be(24)
+  //		}
+  //		it("has a length"){
+  //			Cons(2,Cons(3,Nil)).length should be(2)
+  //		}
+  //		it("add one to a list of ints"){
+  //			val result = List.addOne(Cons(2,Cons(3,Nil)))
+  //
+  //			result should be(Cons(3,Cons(4,Nil)))
+  //		}
+  //		it("can render elements as strings"){
+  //			val result = List.tostring(Cons(1,Cons(2,Nil)))
+  //
+  //			result should be(Cons("1",Cons("2",Nil)))
+  //		}
+  //		it("can be filtered"){
+  //			val evens = Cons(1,Cons(2,Cons(3,Nil))).filter(p => p % 2 == 0)
+  //
+  //			evens should be(Cons(2,Nil))
+  //		}
+  //		it("can appended to another list"){
+  //			val list1 = Cons(1,Cons(2,Nil))
+  //			val list2 = Cons(3,Nil)
+  //
+  //			val result = List.append(list1,list2)
+  //
+  //			result should be(Cons(1,Cons(2,Cons(3,Nil))))
+  //		}
+  //		it("can be flatMapped"){
+  //			val list = Cons(1,Cons(2,Nil))
+  //
+  //			val result = list.flatMap(x => Cons(0,Cons(x,Nil)))
+  //
+  //			result should be(Cons(0,Cons(1,Cons(0,Cons(2,Nil)))))
+  //		}
+  //	 }
 }
--- src/test/scala/fr/xebia/xke/fp/OptionSpec.scala
+++ src/test/scala/fr/xebia/xke/fp/OptionSpec.scala
@@ -1,78 +1,77 @@
 package fr.xebia.xke.fp
 
-import org.scalatest.FunSpec
-import org.scalatest.Matchers
+import org.scalatest.{FunSpec, Matchers}
 
 class OptionSpec extends FunSpec with Matchers {
-
-	describe("An empty option"){
-		it("can be mapped"){
-			val o:Option[Int] = None
-			
-			o.map(i => i + 1) should be(None)
-		}
-		it("can be flatMapped"){
-			val o:Option[Int] = None
-			
-			o.flatMap(i => Some(i + 1)) should be(None)
-		}
-		it("return the default value"){
-			val o:Option[Int] = None
-			
-			o.getOrElse(5) should be(5)
-		}
-		it("return the optional default value"){
-			val o:Option[Int] = None
-			
-			o.orElse(Some(5)) should be(Some(5))
-		}
-		it("can be filtered"){
-			val o:Option[Int] = None
-
-			val even = o.filter(i => i % 2 == 0)
-
-			even should be(None)
-		}
-	}
-	describe("A non-empty option"){
-		it("can be mapped"){
-			val o = Some(1)
-			
-			o.map(i => i + 1) should be(Some(2))
-		}
-		it("can be flatMapped with some value"){
-			val o = Some(1)
-			
-			o.flatMap(i => Some(i + 1)) should be(Some(2))
-		}
-		it("can be flatMapped with non value"){
-			val o = Some(1)
-			
-			o.flatMap(i => None) should be(None)
-		}
-		it("return the wrapped value"){
-			val o = Some(1)
-			
-			o.getOrElse(5) should be(1)
-		}
-		it("return itself in orElse"){
-			val o = Some(1)
-			
-			o.orElse(Some(5)) should be(Some(1))
-		}
-		it("can be filtered out"){
-			val o = Some(1)
-
-			val even = o.filter(i => i % 2 == 0)
-
-			even should be(None)
-		}
-		it("can be filtered in"){
-			val o = Some(2)
-
-			val even = o.filter(i => i % 2 == 0)
-
-			even should be(Some(2))
-		}
-	}
+  //
+  //	describe("An empty option"){
+  //		it("can be mapped"){
+  //			val o:Option[Int] = None
+  //
+  //			o.map(i => i + 1) should be(None)
+  //		}
+  //		it("can be flatMapped"){
+  //			val o:Option[Int] = None
+  //
+  //			o.flatMap(i => Some(i + 1)) should be(None)
+  //		}
+  //		it("return the default value"){
+  //			val o:Option[Int] = None
+  //
+  //			o.getOrElse(5) should be(5)
+  //		}
+  //		it("return the optional default value"){
+  //			val o:Option[Int] = None
+  //
+  //			o.orElse(Some(5)) should be(Some(5))
+  //		}
+  //		it("can be filtered"){
+  //			val o:Option[Int] = None
+  //
+  //			val even = o.filter(i => i % 2 == 0)
+  //
+  //			even should be(None)
+  //		}
+  //	}
+  //	describe("A non-empty option"){
+  //		it("can be mapped"){
+  //			val o = Some(1)
+  //
+  //			o.map(i => i + 1) should be(Some(2))
+  //		}
+  //		it("can be flatMapped with some value"){
+  //			val o = Some(1)
+  //
+  //			o.flatMap(i => Some(i + 1)) should be(Some(2))
+  //		}
+  //		it("can be flatMapped with non value"){
+  //			val o = Some(1)
+  //
+  //			o.flatMap(i => None) should be(None)
+  //		}
+  //		it("return the wrapped value"){
+  //			val o = Some(1)
+  //
+  //			o.getOrElse(5) should be(1)
+  //		}
+  //		it("return itself in orElse"){
+  //			val o = Some(1)
+  //
+  //			o.orElse(Some(5)) should be(Some(1))
+  //		}
+  //		it("can be filtered out"){
+  //			val o = Some(1)
+  //
+  //			val even = o.filter(i => i % 2 == 0)
+  //
+  //			even should be(None)
+  //		}
+  //		it("can be filtered in"){
+  //			val o = Some(2)
+  //
+  //			val even = o.filter(i => i % 2 == 0)
+  //
+  //			even should be(Some(2))
+  //		}
+  //	}
 }
